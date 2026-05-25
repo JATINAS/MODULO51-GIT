22f895687139f378a804e5484d89d6ecefd9745c
Jair Portillo <jairpc385@gmail.com>
2026-05-25 17:02:48 -0600

    dreate a simple java class

---
+++ modulo51-Git/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "modulo51-Git"
--- modulo51-Git/src/main/java/PracticaGit.java
+++ modulo51-Git/src/main/java/PracticaGit.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class PracticaGit {
+
+    static void main(String[] args) {
+        System.out.println("Esta es la practica de GIT!!!");
+    }
+
 }
