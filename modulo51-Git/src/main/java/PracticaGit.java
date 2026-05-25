c19187515908a14fa1ce784174366b826024e302
Jair Portillo <jairpc385@gmail.com>
2026-05-25 17:11:02 -0600

    create a new branch and add a message in it

--- modulo51-Git/src/main/java/PracticaGit.java
+++ modulo51-Git/src/main/java/PracticaGit.java
@@ -2,6 +2,8 @@ public class PracticaGit {
 
     static void main(String[] args) {
         System.out.println("Esta es la practica de GIT!!!");
+
+        System.out.println("NOW WE CREATE A NEW BRANCH AND WILL CHANGE THE MAIN BRANCH");
     }
 
 }
